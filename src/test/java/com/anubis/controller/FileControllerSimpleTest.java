a8534ddd7b0f349974170260f672f9c23e6296e3
yojairo <130933544+yojair0@users.noreply.github.com>
2025-10-07 04:50:47 -0300

    fix: remove SpringBootTest from FileControllerSimpleTest to avoid context loading issues

--- src/test/java/com/anubis/controller/FileControllerSimpleTest.java
+++ src/test/java/com/anubis/controller/FileControllerSimpleTest.java
@@ -4,10 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
 class FileControllerSimpleTest {
 
     @Test
